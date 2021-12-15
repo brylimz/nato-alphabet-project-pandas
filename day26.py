0ad0cb5f1b90664a291418cf5fe49c85b65c5e91
brylimz <superuser@brylimz.com>
2021-12-15 14:00:17 +0700

    crazy man

--- day26.py
+++ day26.py
@@ -1,1 +1,15 @@
-a = "list comprehension"
+# numbers = [1, 2, 3]
+# new_numbers = [n + 1 for n in numbers]
+# print(new_numbers)
+
+# new_bobo = []
+# for n in numbers:
+#     hehe = n + 1
+#     new_bobo.append(hehe)
+#     print(new_bobo)
+
+name = "Angela"
+new_list = [letter for letter in name]
+
+new_numbers = [i for i in range(10) if i % 2 == 0]
+print(new_numbers)
