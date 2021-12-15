566f257a0ed1a894d6f109e8abb3d758662ce73c
brylimz <superuser@brylimz.com>
2021-12-15 14:10:13 +0700

    crazy man

--- day26.py
+++ day26.py
@@ -11,5 +11,9 @@
 name = "Angela"
 new_list = [letter for letter in name]
 
-new_numbers = [i for i in range(10) if i % 2 == 0]
-print(new_numbers)
+# new_numbers = [i for i in range(1, 10) if i % 2 == 0]
+# print(new_numbers)
+range_list = [new_item * 2 for new_item in range(1, 5)]
+print(range_list)
+
+name = []
