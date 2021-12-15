802babee4b8a0e18b047a2dae0be4e253aedac0f
brylimz <superuser@brylimz.com>
2021-12-15 15:21:17 +0700

    challenge 3

--- exercise.py
+++ exercise.py
@@ -1,22 +1,33 @@
 import pandas
+
+# # file 1
+# file1 = pandas.read_csv("file1.txt")
+# new_file1 = file1.values.tolist()
+# print(new_file1)
+#
+# # file 2
+# file2 = pandas.read_csv("file2.txt")
+# new_file2 = file2.values.tolist()
+# print(new_file2)
+#
+# # List compare
+# abc = ""
+# result = []
+# print(result)
+#
+#
+# # Write your code above 👆
+
 # file 1
-file1 = pandas.read_csv("file1.txt")
-new_file1 = file1.values.tostring()
-print(new_file1)
+with open("file1.txt", mode="r") as file1:
+    new_format1 = file1.read().splitlines()
+    print(new_format1)
 
 # file 2
-file2 = pandas.read_csv("file2.txt")
-new_file2 = file2.values.tolist()
-print(new_file2)
+with open("file2.txt", mode="r") as file2:
+    new_format2 = file2.read().splitlines()
+    print(new_format2)
 
-# List compare
-abc = ""
-result = []
+# make a list
+result = [i for i in new_format1 and new_format2 if new_format1 and new_format2]
 print(result)
-
-
-# Write your code above 👆
-
-print(result)
-
-
