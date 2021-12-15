67d35e29af3d1a99910b0b7c9ab6774ca3feb10c
brylimz <superuser@brylimz.com>
2021-12-15 15:05:12 +0700

    challenge 3

---
+++ exercise.py
@@ -0,0 +1,21 @@
+import pandas
+# file 1
+file1 = pandas.read_csv("file1.txt")
+new_file1 = file1.values.tostring()
+print(new_file1)
+
+# file 2
+file2 = pandas.read_csv("file2.txt")
+new_file2 = file2.values.tolist()
+print(new_file2)
+
+# List compare
+abc = ""
+print(result)
+
+
+# Write your code above 👆
+
+print(result)
+
+
